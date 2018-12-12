3f098b2b27e72234712cd6cd335dcb2366ba85f8
ztkmkoo <ztkmk@naver.com>
2018-12-12 20:12:37 +0900

    core v0.0.9 - add empty database(null object)

---
+++ core/src/main/java/com/ztkmkoo/purelink/core/database/EmptyDatabase.java
@@ -0,0 +1,24 @@
+package com.ztkmkoo.purelink.core.database;
+
+public class EmptyDatabase extends AbstractDatabase<Integer, Object> {
+
+    @Override
+    public boolean initOnLoad() {
+        return false;
+    }
+
+    @Override
+    public Object save(Integer key, Object value) {
+        return null;
+    }
+
+    @Override
+    public Object getByKey(Integer key) {
+        return null;
+    }
+
+    @Override
+    public Object deleteByKey(Integer key) {
+        return null;
+    }
+}
